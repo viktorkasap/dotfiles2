90d85cc0713645cc2c9258346b13d78ebe885790
Viktor Kasap <kasap.victor@gmail.com>
2025-11-04 17:23:47 -0700

    Update config

--- setup.sh
+++ setup.sh
@@ -14,22 +14,31 @@ brew install starship fzf git-graph eza bat zoxide tmux ripgrep fd neovim stow p
 
 # Install oh-my-zsh plugins
 custom="$HOME/.oh-my-zsh/custom/plugins"
-declare -A plugins=(
-  ["$custom/zsh-autosuggestions"]="https://github.com/zsh-users/zsh-autosuggestions.git"
-  ["$custom/zsh-syntax-highlighting"]="https://github.com/zsh-users/zsh-syntax-highlighting.git"
-  ["$custom/node-bin"]="https://github.com/remcohaszing/zsh-node-bin.git"
-  ["$custom/zsh-bat"]="https://github.com/fdellwing/zsh-bat.git"
-  ["$custom/zsh-nvm"]="https://github.com/lukechilds/zsh-nvm"
-)
 
-for plugin in "${!plugins[@]}"; do
-  [ -d "$plugin/.git" ] || git clone "${plugins[$plugin]}" "$plugin"
-done
+# declare -A plugins=(
+#   ["$custom/zsh-autosuggestions"]="https://github.com/zsh-users/zsh-autosuggestions.git"
+#   ["$custom/zsh-syntax-highlighting"]="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+#   ["$custom/node-bin"]="https://github.com/remcohaszing/zsh-node-bin.git"
+#   ["$custom/zsh-bat"]="https://github.com/fdellwing/zsh-bat.git"
+#   ["$custom/zsh-nvm"]="https://github.com/lukechilds/zsh-nvm"
+# )
+
+# for plugin in "${!plugins[@]}"; do
+#   [ -d "$plugin/.git" ] || git clone "${plugins[$plugin]}" "$plugin"
+# done
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $custom/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $custom/zsh-syntax-highlighting
+git clone https://github.com/remcohaszing/zsh-node-bin.git $custom/node-bin
+git clone https://github.com/fdellwing/zsh-bat.git $custom/zsh-bat
+git clone https://github.com/lukechilds/zsh-nvmu $custom/zsh-nvm
+
+
 
 # Ensure Node (LTS) is installed & active
-nvm install --lts 
+nvm install --lts
 
-# Install script for Catppuccin Tmux 
+# Install script for Catppuccin Tmux
 dir=$HOME/.config/tmux/plugins/catppuccin/tmux
 [ -d "$dir/.git" ] || git clone -b v2.1.3 https://github.com/catppuccin/tmux.git "$dir"
 
@@ -55,7 +64,7 @@ for config in "${configs[@]}"; do
   [ -e "$config" ] && mv "$config" "$config.backup"
 done
 
-# Apply simlinks 
+# Apply simlinks
 stow -t $HOME -d "$HOME/dotfiles" --ignore "README.md" --ignore "setup.sh" .
 
 # Install tmux plugins
